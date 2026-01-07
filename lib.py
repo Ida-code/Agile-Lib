e1cea249b03cca7bcae50ad45393e22950254e89
Ida <idasharon.r@gmail.com>
2026-01-07 20:59:46 +0530

    Changes by dev1

--- lib.py
+++ lib.py
@@ -3,11 +3,11 @@
 library = []
 students = []
 
-def login():
+def user_login():
     username = input("Enter username: ")
     password = input("Enter password: ")
 
-    if username == "admin" and password == "admin123":
+    if username == "admin" and password == "admin1":
         print("Login successful!\n")
         return True
     else:
@@ -146,7 +146,7 @@ def menu():
             print("Invalid choice. Try again.\n")
 
 
-if login():
+if user_login():
     menu()
 
 
